6e5e2799c16af3f0a2cb2580b180df4c79d24251
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-05-08 16:58:24 -0500

    manejo urls

--- PABB/ABB/urls.py
+++ PABB/ABB/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cursesabb/', include('cursesabb.urls')),
+    path('', include('cursesabb.urls')),
     
 ]
